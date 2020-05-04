0a695647a83ef54314ae3a2989b079daebebbfba
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-05-03 23:50:36 -0700

    wrote function for writing pdf to disk, needs unit testing

--- car_data/scrape.py
+++ car_data/scrape.py
@@ -2,6 +2,21 @@ import PyPDF2 as pypdf
 import requests
 
 
-url = 'https://www.dmv.ca.gov/portal/wcm/connect/fafd3447-8e14-4ff6-bb98-e85f3aa9a207/ca_dmv_stats.pdf?MOD=AJPERES&CVID='
+def download_pdf(url_string):
 
-doc = requests.get(url)
+	doc = requests.get(url)
+	
+	"""if doc.status_code == 200:
+		with open('car_data.pdf', 'wb') as file:
+			file.write(doc.content)
+		return doc
+	else:
+		print("HTTP Response Status Code: " + str(doc.status_code))
+		"""
+	return doc
+
+if __name__ == '__main__':
+	
+	url = 'https://www.dmv.ca.gov/portal/wcm/connect/fafd3447-8e14-4ff6-bb98-e85f3aa9a207/ca_dmv_stats.pdf?MOD=AJPERES&CVID='
+	
+	mypdf = download_pdf(url)
